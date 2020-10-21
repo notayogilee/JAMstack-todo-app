bd9e1babf9d9805935811ccd59e3c56000b3b1b5
Lee Castelani <notayogilee@gmail.com>
2020-10-21 12:13:04 -0400

    add countdown timer for live class

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,27 +1,19 @@
 import React from 'react';
 import { TwitchEmbed, TwitchChat, TwitchClip, TwitchPlayer } from 'react-twitch-embed';
+import Timer from './Timer';
 
 export default function PrivateRoute() {
+
   return (
     <div className="row">
-      {/* <TwitchEmbed
-        channel="notayogilee"
-        id="notayogilee"
-        height="100%"
-        muted
-        onVideoPause={() => console.log(':(')}
-      /> */}
-      {/* <TwitchClip clip="WealthyBumblingKimchiItsBoshyTime" parent={['mycoolsite.com, anotherawesomesite.net']} /> */}
       <div className="container">
-        <div>
-
-          <h4>Next live class on Monday at 6pm</h4>
-          <TwitchChat channel="notayogilee" theme="light" />
+        <div className="col s12">
+          <Timer />
         </div>
         <TwitchPlayer
           channel="notayogilee"
           id="notayogilee"
-          // height="60vh"
+          height="60vh"
           allowFullScreen="true"
           overflow="hidden"
 
