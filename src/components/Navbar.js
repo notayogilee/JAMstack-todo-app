2c5f4df993f2512936735eeff9dfb0c076870abc
Lee Castelani <notayogilee@gmail.com>
2020-10-20 21:18:16 -0400

    change a tag to Link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
       <div className="nav-wrapper green lighten-1">
         <Link to="/" className="brand-logo left">Todo List</Link>
         <ul id="nav-mobile" className="right">
-          <li><a href="/privateRoute">Private Route</a></li>
+          <li><Link to="/privateRoute">Private Route</Link></li>
           <li><a href="#!" onClick={() => netlifyIdentity.open()}>Login/Register</a></li>
         </ul>
       </div>
